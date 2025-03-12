88314377bfd267563e4548143f5a9cc068fcc7d7
beingborn <dolmin0708@naver.com>
2025-03-12 19:03:58 +0900

    review system add , prettier 수정, 각각 리뷰 데이터 수정

--- src/components/productDetail.vue
+++ src/components/productDetail.vue
@@ -2,13 +2,13 @@
     <div class="product-detail">
         <div class="product-detail-left">
             <div class="product-detail-thumb">
-                <img :src="src + '&fm=webp'" loading="lazy" alt="제품 이미지">
+                <img :src="src + '&fm=webp'" loading="lazy" alt="제품 이미지" />
             </div>
         </div>
         <div class="product-detail-right">
             <div class="product-detail-icon pri">
-                <font-awesome-icon v-if="like" :icon="['fas', 'heart']"/>
-                <font-awesome-icon v-else :icon="['far', 'heart']"/>
+                <font-awesome-icon v-if="like" :icon="['fas', 'heart']" />
+                <font-awesome-icon v-else :icon="['far', 'heart']" />
                 <font-awesome-icon :icon="['fas', 'cart-shopping']" />
             </div>
             <div class="product-detail-head">
@@ -16,13 +16,15 @@
                 <p>{{ desc }}</p>
                 <div class="product-detail-price">
                     <span>
-                        <strong>{{ price }}</strong>원
-                    </span>    
+                        <strong>{{ price }}</strong
+                        >원
+                    </span>
                 </div>
             </div>
             <div class="product-detail-body">
                 <button class="btn-lg bg-pri" type="button">구매하기</button>
                 <button class="btn-lg bg-gray" type="button">후기</button>
+                <button class="btn-lg bg-gray" type="button">후기 작성</button>
             </div>
         </div>
     </div>
@@ -30,58 +32,116 @@
         <div class="section-header">
             <h2 class="section-header-tit">후기</h2>
             <span>
-                <strong>135</strong>개
+                <strong id="review">{{ review.length }}</strong
+                >개
             </span>
         </div>
+        <div class="product-review">
+            <ul>
+                <li v-for="(item, index) in review" :key="index">
+                    <span>{{ item.username }}</span>
+                    <span>{{ item.review }}</span>
+                </li>
+            </ul>
+        </div>
     </div>
-
 </template>
 
 <script>
-import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'
 
 export default {
-    name : 'product detail',
-    data(){
+    name: 'product detail',
+    data() {
         return {
-            title : null,
+            title: null,
         }
     },
 
     // Created
     // 컴포넌트 생성 시 + DOM이 렌더링 되기 전 실행됨 (템플릿 접근 X)
 
-    // 1. 초기 데이터 
+    // 1. 초기 데이터
     // 2. 라우트 데이터
     created() {
-        this.title = history.state.title;  
-        this.desc  = history.state.desc;
-        this.price = history.state.price;
-        this.src  = history.state.src;
-        this.like  = history.state.like;
-    }
-};
+        this.title = history.state.title
+        this.desc = history.state.desc
+        this.price = history.state.price
+        this.src = history.state.src
+        this.like = history.state.like
+        this.review = JSON.parse(history.state.review)
+    },
+}
 </script>
 
 <style>
-    /* Product Detail Left */
-    .product-detail { display: flex; padding-top: 40px; align-items: center; padding-bottom: 40px; border-bottom: 1px solid #CCC;}
-    .product-detail-left { flex-grow: 0; flex-shrink: 0; flex-basis: 50%; padding-right: 40px;}
-    .product-detail-thumb { width: 100%; height: auto; border-radius: 4px; overflow: hidden;}
-    .product-detail-thumb > img { width: 100%; object-fit: cover; }
+/* Product Detail Left */
+.product-detail {
+    display: flex;
+    padding-top: 40px;
+    align-items: center;
+    padding-bottom: 40px;
+    border-bottom: 1px solid #ccc;
+}
+.product-detail-left {
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 50%;
+    padding-right: 40px;
+}
+.product-detail-thumb {
+    width: 100%;
+    height: auto;
+    border-radius: 4px;
+    overflow: hidden;
+}
+.product-detail-thumb > img {
+    width: 100%;
+    object-fit: cover;
+}
 
-    /* Product Detail Right */
-    .product-detail-right { flex-grow: 1; }
-    .product-detail-icon > svg { width: 28px; height: 28px;}
-    .product-detail-icon > svg + svg { margin-left: 12px; }
+/* Product Detail Right */
+.product-detail-right {
+    flex-grow: 1;
+}
+.product-detail-icon > svg {
+    width: 28px;
+    height: 28px;
+}
+.product-detail-icon > svg + svg {
+    margin-left: 12px;
+}
 
-    /* Product Detail Head */
-    .product-detail-head {margin-top: 12px;}
-    .product-detail-head > h2 {font-size: 28px;}
-    .product-detail-head > p {font-size: 20px; margin-top: 8px;}
-    .product-detail-head .product-detail-price > span {font-size: 28px;}
+/* Product Detail Head */
+.product-detail-head {
+    margin-top: 12px;
+}
+.product-detail-head > h2 {
+    font-size: 28px;
+}
+.product-detail-head > p {
+    font-size: 20px;
+    margin-top: 8px;
+}
+.product-detail-head .product-detail-price > span {
+    font-size: 28px;
+}
 
-    /* Product Detail Body */
-    .product-detail-body > button {margin-top: 32px; width: 180px;}
-    .product-detail-body > button + button {margin-left: 8px;}
+/* Product Detail Body */
+.product-detail-body > button {
+    margin-top: 32px;
+    width: 180px;
+}
+.product-detail-body > button + button {
+    margin-left: 8px;
+}
+
+/* Product Section Header */
+.section-header {
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
 </style>
--- src/components/productList.vue
+++ src/components/productList.vue
@@ -1,63 +1,97 @@
 <template>
-        <ul class="tab-list">
-            <li role="tab"
-                v-for="(tab, index) in tabList"
+    <ul class="tab-list">
+        <li
+            role="tab"
+            v-for="(tab, index) in tabList"
+            :key="index"
+            :class="{ 'is-active': activeTab === index }"
+            @click="changeTab(index)"
+        >
+            <button type="button">
+                <font-awesome-icon :icon="tab.icon" />
+                {{ tab.name }}
+            </button>
+        </li>
+    </ul>
+    <div
+        class="tab-content"
+        v-for="(tab, index) in tabList"
+        :key="index"
+        :class="{ 'is-active': activeTab === index }"
+    >
+        <ul class="product-list">
+            <li
+                class="product-item"
+                v-for="(item, index) in tab.content"
                 :key="index"
-                :class="{'is-active' : activeTab === index}"
-                @click="changeTab(index)"
-                >
-                <button type="button">
-                    <font-awesome-icon :icon="tab.icon" />
-                    {{ tab.name }}
-                </button>
+            >
+                <div class="product-item-in">
+                    <div
+                        class="product-thumb"
+                        @click="routePushData(item, index)"
+                    >
+                        <img
+                            :src="item.image + '&fm=webp'"
+                            loading="lazy"
+                            v-if="imageLoaded[index]"
+                            alt="제품 이미지"
+                        />
+                        <div v-else class="image-loading">...Loading</div>
+                    </div>
+                    <div class="product-head">
+                        <div class="product-head-top">
+                            <p class="product-head-brand">{{ item.brand }}</p>
+                            <div class="product-thumb-icon pri">
+                                <font-awesome-icon
+                                    v-if="item.like"
+                                    :icon="['fas', 'heart']"
+                                    @click="item.like = !item.like"
+                                />
+                                <font-awesome-icon
+                                    v-else
+                                    :icon="['far', 'heart']"
+                                    @click="item.like = !item.like"
+                                />
+                                <font-awesome-icon
+                                    :icon="['fas', 'cart-shopping']"
+                                />
+                            </div>
+                        </div>
+                        <h2
+                            class="product-head-tit"
+                            @click="$router.push(`productdetail/${index}`)"
+                        >
+                            {{ item.title }}
+                        </h2>
+                        <span class="product-head-price"
+                            >{{ item.price }}원</span
+                        >
+                    </div>
+                    <div class="product-body">
+                        <p class="product-body-desc">{{ item.desc }}</p>
+                    </div>
+                </div>
             </li>
         </ul>
-        <div class="tab-content" v-for="(tab, index) in tabList" :key="index" :class="{'is-active' : activeTab === index}">
-            <ul class="product-list">
-                <li class="product-item" v-for="(item, index) in tab.content" :key="index">
-                    <div class="product-item-in">
-                        <div 
-                        class="product-thumb" 
-                        @click="routePushData(item, index)"
-                        >
-                            <img :src="item.image + '&fm=webp'" loading="lazy" v-if="imageLoaded[index]"  alt="제품 이미지">
-                            <div v-else class="image-loading">...Loading</div>
-                        </div>
-                        <div class="product-head">
-                            <div class="product-head-top">
-                                <p class="product-head-brand">{{ item.brand }}</p>
-                                <div class="product-thumb-icon pri">
-                                    <font-awesome-icon v-if="item.like" :icon="['fas', 'heart']" @click="item.like = !item.like"/>
-                                    <font-awesome-icon v-else :icon="['far', 'heart']" @click="item.like = !item.like"/>
-                                    <font-awesome-icon :icon="['fas', 'cart-shopping']" />
-                                </div>
-                            </div>
-                            <h2 class="product-head-tit" @click="$router.push(`productdetail/${index}`)">{{ item.title }}</h2>
-                            <span class="product-head-price">{{ item.price}}원</span>
-                        </div>
-                        <div class="product-body">
-                            <p class="product-body-desc">{{item.desc}}</p>
-                        </div>
-                    </div>
+        <div class="paging">
+            <button type="button" class="prev">
+                <font-awesome-icon :icon="['fas', 'chevron-left']" />
+                이전
+            </button>
+            <ul class="paging-list">
+                <li class="paging-link is-active">
+                    <button type="button">1</button>
                 </li>
+                <li class="paging-link"><button type="button">2</button></li>
+                <li class="paging-link"><button type="button">3</button></li>
+                <li class="paging-link"><button type="button">4</button></li>
             </ul>
-            <div class="paging">
-                <button type="button" class="prev">
-                    <font-awesome-icon :icon="['fas', 'chevron-left']" />
-                    이전
-                </button>
-                <ul class="paging-list">
-                    <li class="paging-link is-active"><button type="button">1</button></li>
-                    <li class="paging-link"><button type="button">2</button></li>
-                    <li class="paging-link"><button type="button">3</button></li>
-                    <li class="paging-link"><button type="button">4</button></li>
-                </ul>
-                <button type="button" class="next">
-                    다음
-                    <font-awesome-icon :icon="['fas', 'chevron-right']" />
-                </button>
-            </div>
+            <button type="button" class="next">
+                다음
+                <font-awesome-icon :icon="['fas', 'chevron-right']" />
+            </button>
         </div>
+    </div>
 </template>
 
 <script>
@@ -68,114 +102,243 @@ import mouse from '../assets/data/mouse'
 import accessories from '../assets/data/accessories'
 
 export default {
-    name : 'Container',
-    data(){
+    name: 'Container',
+    data() {
         return {
-            tabList : [
-                {name: '키보드', content : keyboard, icon: ['far', 'keyboard']},
-                {name: '헤드셋', content: headset, icon:  ['fas', 'headphones']},
-                {name: '스피커', content: speaker, icon:  ['fas', 'volume-high']},
-                {name: '액세서리', content: accessories, icon:  ['fas', 'plug']},
-                {name: '마우스', content: mouse, icon:  ['fas', 'computer-mouse']},
+            tabList: [
+                {
+                    name: '키보드',
+                    content: keyboard,
+                    icon: ['far', 'keyboard'],
+                },
+                {
+                    name: '헤드셋',
+                    content: headset,
+                    icon: ['fas', 'headphones'],
+                },
+                {
+                    name: '스피커',
+                    content: speaker,
+                    icon: ['fas', 'volume-high'],
+                },
+                {
+                    name: '액세서리',
+                    content: accessories,
+                    icon: ['fas', 'plug'],
+                },
+                {
+                    name: '마우스',
+                    content: mouse,
+                    icon: ['fas', 'computer-mouse'],
+                },
             ],
-            activeTab : 0,
-            activeTabPosts : [],
-            activeTabCurrent : 1, // 현재 위치
-            activeTabPages : 10, // 보여줄 갯수
-            imageLoaded : [],
+            activeTab: 0,
+            activeTabPosts: [],
+            activeTabCurrent: 1, // 현재 위치
+            activeTabPages: 10, // 보여줄 갯수
+            imageLoaded: [],
         }
     },
-    methods : {
-        changeTab(index){
-            this.activeTab = index;
-        },  
+    methods: {
+        changeTab(index) {
+            this.activeTab = index
+        },
 
         // 배열 값을 가져온 후. 해당 값에 src값을 로딩 되었을 때 => onload 배열에 true로 추가
-        tabArrayReturn(){
-            for (let i = 0; i < this.tabList.length; i++){
-                return this.tabList[i].content    
+        tabArrayReturn() {
+            for (let i = 0; i < this.tabList.length; i++) {
+                return this.tabList[i].content
             }
         },
-        imgLoad(){
+        imgLoad() {
             const tabArray = this.tabArrayReturn()
 
             // 로드된 UI만 true값으로 배열에 추가
             tabArray.forEach((item, index) => {
-                const img = new Image();
-                img.src = item.image;
+                const img = new Image()
+                img.src = item.image
 
                 img.onload = () => {
-                    this.imageLoaded[index] = true;
+                    this.imageLoaded[index] = true
                 }
             })
         },
         // Vue 메서드 내부에서 라우터 사용 시 , this를 붙여줘야함
-        routePushData(item, index){
+        routePushData(item, index) {
             this.$router.push({
-                path  : `productdetail/${index}`, 
-                state : { 
-                    title : item.title,
-                    desc  : item.desc,
-                    price : item.price,
-                    src   : item.image,
-                    like  : item.like
-                }
+                path: `productdetail/${index}`,
+                state: {
+                    title: item.title,
+                    desc: item.desc,
+                    price: item.price,
+                    src: item.image,
+                    like: item.like,
+
+                    // 객체나 배열을 넘길 때는 Json.stringify로 문자열 변환이 필요함
+                    review: JSON.stringify(item.review),
+                },
             })
 
-
-        }
+            console.log(item.review)
+        },
     },
-    mounted(){
-        let tab = document.querySelectorAll('.tab-list li');
-        tab[0].classList.add('is-active');
-        let tabContent = document.querySelectorAll('.tab-content');
-        tabContent[0].classList.add("is-active");
+    mounted() {
+        let tab = document.querySelectorAll('.tab-list li')
+        tab[0].classList.add('is-active')
+        let tabContent = document.querySelectorAll('.tab-content')
+        tabContent[0].classList.add('is-active')
 
-        this.imgLoad();
-    }
+        this.imgLoad()
+    },
 }
 </script>
 
 <style>
-    /* Tab */
-    .tab-list {display: flex; gap: 20px; border-top: 1px solid #ccc; border-bottom: 1px solid #ccc; padding: 12px 0;}
-    .tab-list > li.is-active :where(svg, button) {color: var(--pri)}
-    .tab-list > li > svg {width: 20px; height: 20px;}
-    .tab-list > li > button {font-size: 15px; display: flex; flex-direction: column; gap: 4px; align-items: center;}
-    .tab-content {display: none; margin-top: 40px;}
-    .tab-content.is-active {display: block;}
+/* Tab */
+.tab-list {
+    display: flex;
+    gap: 20px;
+    border-top: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    padding: 12px 0;
+}
+.tab-list > li.is-active :where(svg, button) {
+    color: var(--pri);
+}
+.tab-list > li > svg {
+    width: 20px;
+    height: 20px;
+}
+.tab-list > li > button {
+    font-size: 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    align-items: center;
+}
+.tab-content {
+    display: none;
+    margin-top: 40px;
+}
+.tab-content.is-active {
+    display: block;
+}
 
-    /* Product */
-    .product-list {display: flex; flex-wrap: wrap; column-gap: 20px; row-gap: 24px;}
-    .product-item {flex: 1 1 calc((100% - 60px) / 4); text-align: left; max-width: calc((100% - 60px) / 4);}
-    .product-item .product-thumb {align-content: center; text-align: center; border: 1px solid #d8d8d8; border-radius: 12px; overflow: hidden; height: 200px;}
-    .product-item:hover .product-thumb > img {transition: all 300ms ease-in-out;}
-    .product-item:hover .product-thumb > img {filter: brightness(70%);}
-    
-    /* Product Detail */
-    .product-head {width: 100%;}
-    .product-head-top {display: flex; justify-content: space-between; align-items: center; margin-top: 8px; margin-bottom: 8px;}
-    .product-head .product-head-tit {font-weight: 700; font-size: 24px; white-space: nowrap; overflow: hidden; width: 99%; text-overflow: ellipsis; cursor: pointer;} 
-    .product-head .product-head-price {font-size: 18px; font-weight: 500; margin: 8px 0; display: inline-block;}
-    .product-head .product-head-brand {font-size: 20px; font-weight: 700; color: #dfdfdf; margin-top: 8px; margin-bottom: 4px; }
-    .product-body .product-body-desc {font-size: 16px;}
+/* Product */
+.product-list {
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 20px;
+    row-gap: 24px;
+}
+.product-item {
+    flex: 1 1 calc((100% - 60px) / 4);
+    text-align: left;
+    max-width: calc((100% - 60px) / 4);
+}
+.product-item .product-thumb {
+    align-content: center;
+    text-align: center;
+    border: 1px solid #d8d8d8;
+    border-radius: 12px;
+    overflow: hidden;
+    height: 200px;
+}
+.product-item:hover .product-thumb > img {
+    transition: all 300ms ease-in-out;
+}
+.product-item:hover .product-thumb > img {
+    filter: brightness(70%);
+}
 
-    /* Product Thumb */
-    .product-thumb {position: relative;}
-    .product-thumb > img {object-fit: cover; width: 100%; height: 100%; object-position: 50% 50%; cursor: pointer;}
+/* Product Detail */
+.product-head {
+    width: 100%;
+}
+.product-head-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 8px;
+    margin-bottom: 8px;
+}
+.product-head .product-head-tit {
+    font-weight: 700;
+    font-size: 24px;
+    white-space: nowrap;
+    overflow: hidden;
+    width: 99%;
+    text-overflow: ellipsis;
+    cursor: pointer;
+}
+.product-head .product-head-price {
+    font-size: 18px;
+    font-weight: 500;
+    margin: 8px 0;
+    display: inline-block;
+}
+.product-head .product-head-brand {
+    font-size: 20px;
+    font-weight: 700;
+    color: #dfdfdf;
+    margin-top: 8px;
+    margin-bottom: 4px;
+}
+.product-body .product-body-desc {
+    font-size: 16px;
+}
 
-    /* Product Icon */
-    .product-thumb-icon {padding-right: 12px;}
+/* Product Thumb */
+.product-thumb {
+    position: relative;
+}
+.product-thumb > img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    object-position: 50% 50%;
+    cursor: pointer;
+}
 
-    [class*=-icon].pri > svg {color: var(--pri);} 
+/* Product Icon */
+.product-thumb-icon {
+    padding-right: 12px;
+}
 
-    .product-thumb-icon > svg {width: 20px; height: 20px; cursor: pointer;}
-    .product-thumb-icon > svg + svg {margin-left: 8px;}
+[class*='-icon'].pri > svg {
+    color: var(--pri);
+}
 
-    /* Paging */
-    .paging {display: flex; gap: 12px; justify-content: center; margin-top: 48px;}
-    .paging :is(button) {font-size: 20px;}
-    .paging-list {display: flex; gap: 8px;}
-    .paging-link > button {width: 36px; aspect-ratio: 1; border-radius: 8px;}
-    .paging-link.is-active > button {background: var(--pri); color: white;}
+.product-thumb-icon > svg {
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+}
+.product-thumb-icon > svg + svg {
+    margin-left: 8px;
+}
+
+/* Paging */
+.paging {
+    display: flex;
+    gap: 12px;
+    justify-content: center;
+    margin-top: 48px;
+}
+.paging :is(button) {
+    font-size: 20px;
+}
+.paging-list {
+    display: flex;
+    gap: 8px;
+}
+.paging-link > button {
+    width: 36px;
+    aspect-ratio: 1;
+    border-radius: 8px;
+}
+.paging-link.is-active > button {
+    background: var(--pri);
+    color: white;
+}
 </style>
